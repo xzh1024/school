82cc8e29eca9aadee58ec0e57cd00bea1eb879f5
xiezihan <zihan.xie@hztl3.com>
2022-03-23 15:05:29 +0800

    update

--- react/heima/05-react-component/src/index.js
+++ react/heima/05-react-component/src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  // constructor() {
+  // super虽然代表了父类A的构造函数，但是返回的是子类B的实例，即super内部的this指的是B的实例，因此super()在这里相当于A.prototype.constructor.call(this)。
+  //   super();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //   // 初始化state
+  //   this.state = {
+  //     count: 0,
+  //   };
+  // }
+
+  // 简化语法初始化state
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>计数器：{this.state.count}</h1>
+        <button onClick={this.add}>+1</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
