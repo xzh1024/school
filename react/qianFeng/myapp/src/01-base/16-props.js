153c9810bfb59e6f25e0ce6593fed74d2be2e06d
xiezihan <xiezihan@dgg.net>
2022-06-15 09:53:06 +0800

    1

--- react/qianFeng/myapp/src/01-base/16-props.js
+++ react/qianFeng/myapp/src/01-base/16-props.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+// import propTypes from 'prop-types';
 import Navbar from "./Navbar/index";
+import "./css/03-layout.scss";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        App
-        <Navbar id={1}></Navbar>
+      <div className="container">
+        <div className="main">App</div>
+        <Navbar title="首页" leftShow={false}></Navbar>
       </div>
     );
   }
 }
+
+// 类属性
+// Navbar.propTypes = {
+//   title: propTypes.string.isRequired, // 验证是不是字符串
+//   leftShow: propTypes.bool.isRequired // 验证是不是bool
+// }
--- react/qianFeng/myapp/src/01-base/Navbar/index.js
+++ react/qianFeng/myapp/src/01-base/Navbar/index.js
@@ -1,34 +1,54 @@
 import React, { Component } from "react";
-import "./index.css";
+import "./index.scss";
+import propTypes from "prop-types";
+console.log(propTypes);
 
 export default class Navbar extends Component {
+  // 静态属性  类型检测
+  static propTypes = {
+    title: propTypes.string.isRequired,
+  };
+
   state = {
     list: [
       {
-        name: "首页",
+        title: "首页",
         id: 1,
       },
       {
-        name: "列表",
+        title: "列表",
         id: 2,
       },
       {
-        name: "购物车",
+        title: "购物车",
         id: 3,
       },
     ],
   };
+
   render() {
     console.log(this.props);
-    const { id } = this.props;
+    const { title, leftShow } = this.props;
+    console.log(leftShow);
     return (
-      <div>
+      <div className="navbar">
         {this.state.list.map((item) => (
-          <div key={item.id} className={id === item.id ? "active" : ""}>
-            {item.name}
+          <div
+            key={item.id}
+            className={
+              title === item.title ? "navbar-item active" : "navbar-item"
+            }
+          >
+            {item.title}
           </div>
         ))}
       </div>
     );
   }
 }
+
+// 指定props的默认值，这个方法会一直生效
+Navbar.defaultProps = {
+  title: "购物车",
+  leftShow: true,
+};
