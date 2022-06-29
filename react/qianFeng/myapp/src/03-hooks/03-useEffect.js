e1e44c926110882a4ac1cbd98eb8cac3d582477c
xiezihan <xiezihan@dgg.net>
2022-06-29 14:19:12 +0800

    fqt

--- react/qianFeng/myapp/src/03-hooks/03-useEffect.js
+++ react/qianFeng/myapp/src/03-hooks/03-useEffect.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 /*
   你之前可能已经在 React 组件中执行过数据获取、订阅或者手动修改过 DOM。
   我们统一把这些操作称为“副作用”，或者简称为“作用”。
 */
 export default function App() {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    axios.get('/test.json').then((res) => {
+      console.log(res.data);
+      setList(res.data);
+      console.log(list);
+    });
+  }, []);
+
   const [count, setCount] = useState(0);
   // 相当于 componentDidMount 和 componentDidUpdate:
   useEffect(() => {
@@ -23,6 +33,12 @@ export default function App() {
         add
       </button>
       <div>{count}</div>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.filmId}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
