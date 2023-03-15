bca33245a06acecc2e194ca78c2d089580912724
xiezihan <xiezihan@huianrong.com>
2023-03-15 12:30:36 +0800

    1

--- npm/npm_demo/src/App.vue
+++ npm/npm_demo/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
-import { wordToNum } from "xzh1024_npm_test/dist/webpack-numbers";
+import { wordToNum } from "xzh1024_npm_test";
 
 const getWordNum = () => {
   console.log("------------");
