c58b3ef5b31eeda40418b6947585403e3f2665e1
xiezihan <xiezihan@farben.com.cn>
2024-11-12 15:09:13 +0800

    canvas

---
+++ vue/vue3/canvas-dome/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">about</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html,
+body {
+  /* 禁止用户缩放 */
+  overflow: hidden;
+  /* 禁止任何形式的缩放 */
+  touch-action: none;
+  /* 禁止缩放手势 */
+  -ms-touch-action: none;
+}
+</style>
---
+++ vue/vue3/canvas-dome/src/views/HomeView.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="home">
+    <div class="home-content">
+      <div class="content-left">
+        <EpcCanvas
+          ref="refEpcCanvas"
+          :isShowCanvas="isShowCanvas"
+          :tableData="tableData"
+          :selectedRows="selectedRows"
+          @onTableData="() => {}"
+          @onSelectedRows="() => {}"
+          :url="imgUrl"
+          style="height: 100%"
+        />
+      </div>
+      <div class="content-right">
+        <div
+          v-for="item in tableData"
+          :key="item.id"
+          class="item"
+          :class="['item', item.id === selectedRows[0]?.id ? 'active' : '']"
+          @click="handleClick(item)"
+        >
+          {{ item.name }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EpcCanvas from '@/components/EpcCanvas.vue'
+// import { Row, Col } from 'element-ui';
+
+export default {
+  name: 'HomeView',
+  components: {
+    EpcCanvas,
+  },
+  data () {
+    return {
+      // imgUrl: '/test.png',
+      // isShowCanvas: true,
+      tableData: [
+        {
+          id: 1,
+          name: '图形1',
+          partRefOnImage: 1,
+          position: [10, 100, 200, 200]
+        },
+        {
+          id: 2,
+          name: '图形2',
+          partRefOnImage: 2,
+          position: [300, 100, 200, 200]
+        },
+        {
+          id: 3,
+          name: '图形3',
+          partRefOnImage: 3,
+          position: [600, 100, 200, 200]
+        },
+      ],
+      selectedRows: [
+      ]
+    }
+  },
+  methods: {
+    handleClick (item) {
+      this.selectedRows = [item]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home-content {
+  display: flex;
+  .content-left {
+    flex: 1;
+  }
+  .content-right {
+    flex: 1;
+    .item {
+      line-height: 40px;
+      font-size: 16px;
+      cursor: pointer;
+      &.active {
+        color: royalblue;
+      }
+    }
+  }
+}
+</style>
